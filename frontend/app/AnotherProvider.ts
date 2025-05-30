eb38f25504f2d19de40655396e5b453762517db9
Corentin Renard <corentinfox08@gmail.com>
2025-05-30 09:33:01 +0200

    chore: cleaned codebase

--- frontend/app/AnotherProvider.ts
+++
@@ -1,3 +0,0 @@
-export default function SomeProvider({ children }: any) {
-  return children;
-}
--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,42 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { Platform } from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
-import { useRouter } from 'expo-router';
+import { Stack } from 'expo-router';
 import { runAllTheInitStuff } from './init';
 import WebSplashScreen from './WebSplashScreen';
 
-// Garder le splash natif visible
 SplashScreen.preventAutoHideAsync();
 
-export default function App() {
+export default function Layout() {
   const [appIsReady, setAppIsReady] = useState(false);
-  const router = useRouter();
 
-  // Toujours appeler les hooks en haut du composant !
   useEffect(() => {
     async function prepare() {
       await runAllTheInitStuff();
       setAppIsReady(true);
       await SplashScreen.hideAsync();
     }
-
     prepare();
   }, []);
 
-  useEffect(() => {
-    if (appIsReady) {
-      router.replace('/(tabs)');
-    }
-  }, [appIsReady]); 
-
-  // Affichage pendant le chargement
   if (!appIsReady) {
-    if (Platform.OS === 'web') {
-      return <WebSplashScreen />;
-    } else {
-      return null;
-    }
+    return Platform.OS === 'web' ? <WebSplashScreen /> : null;
   }
 
-  return null;
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
